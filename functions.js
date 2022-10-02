59ea92b62121e7401e6c25bf5321fa76af391436
manuel.novella <manuel.novella@zurich.com>
2022-10-02 02:19:35 +0200

    Added color

--- functions.js
+++ functions.js
@@ -6,50 +6,80 @@ f(z) = z^2 + c, where z is an imaginary variable and c is the original value of 
 the function.
 */
 
-function iterate_complex_plane() {
+function iterate_complex_plane(max_iterations) {
 
-    loadPixels()
+    //create data structure to store iteration outputs
+    let complex_image = new Array(width)
+    for (let column = 0; column < width; column++) {
+        complex_image[column] = new Array(height)
+    }
+
+    //max value in javascript, so pixels can be painted later according to range
+    let max_value = 1.7e+308
 
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
 
             //transform canvas coordinates to complex plane coordinates
-            let a = map(x, 0, width, -2, 1)
-            let b = map(y, 0, height, -1.5, 1.5)
+            let a = map(x, 0, width, -2/1, 1/1)
+            let b = map(y, 0, height, -1.5/1, 1.5/1)
             let c_a = a //original real component of the point being iterated over
             let c_b = b //original imaginary component of the point being iterated over
 
-            let max_iterations = 400
-            let threshold_value = 16
-            let iteration
-
-            for (iteration = 0; iteration < max_iterations; iteration++) {
+            let i
+            for (i = 0; i < max_iterations; i++) {
                 let a_next = a*a - b*b
                 let b_next = 2*a*b
 
                 a = a_next + c_a
                 b = b_next + c_b
 
-                //if the point in the complex plane goes to infinity
-                if (abs(a + b) > threshold_value) {
+                //calculate modulus value of complex number
+                modulus = Math.sqrt(a*a + b*b)
+                if (modulus > max_value) {
                     break
                 }
             }
 
-            //if the point goes to infinity faster, it'll be darker
-            let brightness = map(iteration+30, 0, max_iterations, 0, 255, true)
+            //store iteration in which "infinity" was reached for each complex number
+            complex_image[x][y] = i
+        }
+    }
 
-            //if the point absolutely goes to infinity, be it dark
-            if(iteration == max_iterations) {
-                brightness = 0
+    return complex_image
+}
+
+
+function display_complex_image(complex_image, max_iterations, phase) {
+    
+    colorMode(HSB)
+
+    loadPixels()
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+
+            value = complex_image[x][y]
+            idx = (x + y * width) * 4
+            
+            //if the point belongs in the Mandelbrot set
+            if (value == max_iterations) {
+                //paint it black
+                pixels[idx + 0] = 0
+                pixels[idx + 1] = 0
+                pixels[idx + 2] = 0
+                pixels[idx + 3] = 255
+            } else {
+                //paint it a color depending how fast it diverges
+                let hue_ = map(value, 0, max_iterations, 0, 360)
+                //let phase = 300   //"degrees" in the cyclical hue wheel
+                hue_ = (hue_ + phase) % 360
+                let color_ = color(hue_, 100, 100, 1)
+                pixels[idx + 0] = red(color_)
+                pixels[idx + 1] = green(color_)
+                pixels[idx + 2] = blue(color_)
+                pixels[idx + 3] = 255 //opacity
             }
-
-            let pixel_idx = (y * width + x) * 4
-            pixels[pixel_idx + 0] = brightness
-            pixels[pixel_idx + 1] = brightness
-            pixels[pixel_idx + 2] = brightness
-            pixels[pixel_idx + 3] = 255
-
         }
     }
 
--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
+var max_iterations = 50
+var phase = 0
+var max_iterations = 50
+
+var complex_image
+
 function setup() {
-  canvas = createCanvas(600, 600)
+  let canvas = createCanvas(600, 600)
   pixelDensity(1)
   background(0)
 
-  iterate_complex_plane()
-  noLoop
-
+  complex_image = iterate_complex_plane(max_iterations)
+  display_complex_image(complex_image, max_iterations, phase)
+  //noLoop
+  
   //timestamp = Date.now().toString()
   //saveCanvas(canvas, "outputs/" + timestamp, "png")
 }
 
 
 function draw() {
-  
+  phase += 2
+  display_complex_image(complex_image, max_iterations, phase)
 }
